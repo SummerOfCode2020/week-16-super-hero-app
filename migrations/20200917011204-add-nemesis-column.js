bba357df2bef17c62faf2188b7d9760071b534fa
Stephanie Bucharelli <stephaniebucharelli@gmail.com>
2020-09-17 19:34:39 -0400

    Try to pull

--- migrations/20200917011204-add-nemesis-column.js
+++ migrations/20200917011204-add-nemesis-column.js
@@ -5,7 +5,7 @@ module.exports = {
     /*
       Add altering commands here.
       Return a promise to correctly handle asynchronicity.
-
+ 
       Example:
       return queryInterface.createTable('users', { id: Sequelize.INTEGER });
     */
@@ -23,7 +23,7 @@ module.exports = {
     /*
       Add reverting commands here.
       Return a promise to correctly handle asynchronicity.
-
+ 
       Example:
       return queryInterface.dropTable('users');
     */

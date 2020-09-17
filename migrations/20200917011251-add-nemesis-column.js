b98b0942b19dfe099bbdda3ee651409e5bcbe3e1
Alvin <fulmar38251@letterboxes.org>
2020-09-16 21:42:04 -0400

    Add nemesis column

---
+++ migrations/20200917011251-add-nemesis-column.js
@@ -0,0 +1,26 @@
+'use strict';
+const models = require('../models')
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+    return queryInterface.addColumn('heroes', 'nemesis', { type: Sequelize.STRING, defaultValue: '' })
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+    return queryInterface.removeColumn('heroes', 'nemesis')
+  }
+};

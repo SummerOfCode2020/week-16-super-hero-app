5acf0cf2f43b6ecc923d0530eb7b0e7c394acff0
Hollis <hollisjamison@gmail.com>
2020-09-16 21:33:32 -0400

    Implements nemesis column

--- models/heroes.js
+++ models/heroes.js
@@ -1,12 +1,17 @@
 const heroes = (connection, Sequelize) => {
-  return connection.define('heroes', {
-    id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
-    name: { type: Sequelize.STRING },
-    realname: { type: Sequelize.STRING },
-    firstappearance: { type: Sequelize.STRING },
-    slug: { type: Sequelize.STRING, unique: true },
-    snapped: { type: Sequelize.BOOLEAN, defaultValue: 0 },
-  }, { paranoid: true })
+  return connection.define(
+    "heroes",
+    {
+      id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
+      name: { type: Sequelize.STRING },
+      realname: { type: Sequelize.STRING },
+      firstappearance: { type: Sequelize.STRING },
+      slug: { type: Sequelize.STRING, unique: true },
+      snapped: { type: Sequelize.BOOLEAN, defaultValue: 0 },
+      nemesis: { type: Sequelize.STRING },
+    },
+    { paranoid: true }
+  );
 }
 
 module.exports = heroes

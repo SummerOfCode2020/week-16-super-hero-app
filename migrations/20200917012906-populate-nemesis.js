b098b5e17b5cfaea4e28366013b27ed7af847c1e
Nick Lavery <tuneboynick@gmail.com>
2020-09-17 19:42:26 -0400

    Populate nemesis

--- migrations/20200917012906-populate-nemesis.js
+++ migrations/20200917012906-populate-nemesis.js
@@ -1,7 +1,7 @@
-'use strict';
+'use strict'
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
+  up: (queryInterface) => {
     /*
       Add altering commands here.
       Return a promise to correctly handle asynchronicity.
@@ -9,7 +9,33 @@ module.exports = {
       Example:
       return queryInterface.createTable('users', { id: Sequelize.INTEGER });
     */
-   
+    return queryInterface.bulkInsert('heroes', [
+      {
+        name: 'Captain America',
+        realname: 'Steve Rogers',
+        firstappearance: 'Captain America Comics #1',
+        slug: 'captain-america',
+        nemesis: 'Red Skull'
+      }, {
+        name: 'Spider-Man',
+        realname: 'Peter Parker',
+        firstappearance: 'Amazing Fantasy #15',
+        slug: 'spider-man',
+        nemesis: 'Green Goblin, et al....'
+      }, {
+        name: 'Iron Man',
+        realname: 'Tony Stark',
+        firstappearance: 'Tales of Suspense #39',
+        slug: 'iron-man',
+        nemesis: 'Obadiah Stane'
+      }, {
+        name: 'Thor',
+        realname: 'Thor Odinson',
+        firstappearance: 'Journey into Mystery #83',
+        slug: 'thor',
+        nemesis: 'Loki'
+      }
+    ])
   },
 
   down: (queryInterface, Sequelize) => {
@@ -21,4 +47,4 @@ module.exports = {
       return queryInterface.dropTable('users');
     */
   }
-};
+}
